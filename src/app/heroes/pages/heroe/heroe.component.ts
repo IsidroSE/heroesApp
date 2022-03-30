326dbdbd978e349cd70347a0d623c0a090f0fb4f
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-03-30 15:35:30 +0200

    add heroe page design

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe } from '../../interfaces/heroes.interface';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
 import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
 
@@ -16,7 +20,8 @@ export class HeroeComponent implements OnInit {
 
   constructor( 
     private activateRoute: ActivatedRoute, 
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -27,6 +32,10 @@ export class HeroeComponent implements OnInit {
     )
     .subscribe( heroe => this.heroe = heroe );
 
+  }
+
+  regresar() {
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
