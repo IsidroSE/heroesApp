e6d2928219f30b78952fc4c923b4de51c8f1f1ab
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-03-30 13:45:23 +0200

    add heroe page

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -15,4 +15,8 @@ export class HeroesService {
     return this.http.get<Heroe[]>('http://localhost:3000/heroes');
   }
 
+  getHeroeById(idHeroe: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${idHeroe}`);
+  }
+
 }
